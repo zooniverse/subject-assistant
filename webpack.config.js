eb0fed9cfa02ef266e362a64d80a9370aafa66f5
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-07-02 18:35:40 +0100

    Fix Webpack Dev Server... sort of

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,33 @@ module.exports = {
   mode: "development",
   module: {
     rules: [
-      {
+      {  // Compile JavaScript files into a single bundle
         test: /\.js$/,
         exclude: /node_modules/,
         use: "babel-loader"
+      },
+      {  // Copy HTML file
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            }
+          },
+        ],
+      },
+      {  // Copy media assets
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets',
+            },
+          },
+        ],
       }
     ]
   },
