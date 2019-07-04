09e670b0d21ec543897c3bd3a5e181c5719f58b5
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-07-04 15:43:43 +0100

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+
+const ComponentX = () => <div>X</div>
+const ComponentY = () => <div>Y</div>
+const ComponentZ = () => <div>Z</div>
 
 class App extends React.Component {
   constructor () {
@@ -9,7 +14,14 @@ class App extends React.Component {
     return (
       <>
         <header>Zooniverse ML Subject Assistant</header>
-        <main>Hello World</main>
+        <main>
+          <h1>Hello World</h1>
+          <Router>
+            <Route path="/x" component={ComponentX} />
+            <Route path="/y" component={ComponentY} />
+            <Route path="/z" component={ComponentZ} />
+          </Router>
+        </main>
         <footer>Footer</footer>
       </>
     )
