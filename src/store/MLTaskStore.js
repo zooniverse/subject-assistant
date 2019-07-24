44a58520de3590334615e6c3996819577d1b4bb0
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-07-24 12:42:38 +0100

    Store MLTaskId in localStorage

--- src/store/MLTaskStore.js
+++ src/store/MLTaskStore.js
@@ -4,12 +4,14 @@ import config from '@config'
 import superagent from 'superagent'
 
 const TASKS_ENDPOINT = '/task'
+const TASK_ID_STORAGE_KEY = 'mlTaskId'
 
 const MLTaskStore = types.model('MLTaskStore', {
   
   status: types.optional(types.string, ASYNC_STATES.IDLE),
-  id: types.optional(types.string, ''),
-  data: types.optional(types.array(types.frozen({})), []),
+  id: types.optional(types.string, localStorage.getItem(TASK_ID_STORAGE_KEY) || ''),  // ID of the ML Task, specified by the user.
+  task: types.optional(types.frozen({}), {}),  // Data related to the ML Task itself.
+  results: types.optional(types.array(types.frozen({})), []),  // Data from the results file, which is linked to from the ML Task.
   
 }).actions(self => {
   return {
@@ -20,6 +22,7 @@ const MLTaskStore = types.model('MLTaskStore', {
     
     setId (val) {
       self.id = val
+      localStorage.setItem(TASK_ID_STORAGE_KEY, val)
     },
     
     fetchTask () {
