97de173022683ea7e3b71ce996544744b0365ce1
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-07-25 18:19:24 +0100

    WIP MLTaskManager now displays a random selection of images. Poorly formatted, alas.

--- src/components/MLTaskManager/DisplayAndSelect.js
+++ src/components/MLTaskManager/DisplayAndSelect.js
@@ -3,6 +3,8 @@ import { observer } from 'mobx-react'
 import AppContext from '@store'
 import { ASYNC_STATES, stopEvent } from '@util'
 
+const NUMBER_OF_SAMPLES = 20
+
 class DisplayAndSelect extends React.Component {
   constructor (props) {
     super(props)
@@ -30,9 +32,35 @@ class DisplayAndSelect extends React.Component {
             <div key={`results-info-${key}`}><b>{key}</b> : <i>{info[key]}</i></div>
           ))}
         </div>
+        {this.renderSampleImages(images)}
       </form>
     )
-  }  
+  }
+
+  renderSampleImages(images = []) {
+    if (!images || !images.length) return
+    
+    // Pick a small sample from the images provided
+    const numOfSamples = Math.min(images.length, NUMBER_OF_SAMPLES)
+    const startIndex = Math.random() * (images.length - numOfSamples)
+    const sampleImages = images.slice(startIndex, startIndex + numOfSamples)
+    
+    return (
+      <div className="list">
+        {sampleImages.map((image, index) => {
+          console.log('+++', image)
+          const imgSrc = image.file || ''
+          
+          return (
+            <img src={imgSrc} className="item" key={`results-image-${index}`} />
+          )
+        })}
+      </div>
+    
+    )
+    
+    
+  }
 }
 
 DisplayAndSelect.contextType = AppContext
