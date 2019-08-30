eae909c8a88a9ec3aedfd08354dfd606e7270765
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-08-30 19:30:09 +0100

    Proxy Server now recognises the path and query of the request

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -1,1 +1,36 @@
-console.log('Hello Proxy Server')
+const express = require('express')
+const superagent = require('superagent')
+
+const server = express()
+
+const config = {
+  port: 3666,
+  targetServer: 'https://shaunanoordin.com',
+}
+
+function getQueryString (queryObject) {
+  const str = Object
+  .keys(queryObject).map((key) => {
+    const val = encodeURIComponent(queryObject[key])
+    return `${key}=${val}`
+  })
+  .join('&')
+  return (str.length > 0)
+    ? `?${str}`
+    : str
+}
+
+function proxyGet (req, res) {
+  const query = getQueryString(req.query)
+  const path = req.path
+  
+  //console.log(res)
+  res.send(`Path: ${path}${query}` )
+}
+
+server.get('*', proxyGet)
+
+server.listen(config.port, (err) => {
+  if (err) throw err
+  console.log(`Proxy Server running at port ${config.port}`)
+})
