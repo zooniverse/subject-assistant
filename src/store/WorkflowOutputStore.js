969843929d63fa47ca989438db3ed6f7e9ba3bba
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-09-12 17:19:28 +0100

    WorkflowOutput.move() now WORKS! IT WOOORKS

--- src/store/WorkflowOutputStore.js
+++ src/store/WorkflowOutputStore.js
@@ -2,6 +2,7 @@ import { flow, types } from 'mobx-state-tree'
 import { ASYNC_STATES } from '@util'
 import config from '@config'
 import apiClient from 'panoptes-client'
+import superagent from 'superagent'
 
 const WorkflowOutputStore = types.model('WorkflowOutputStore', {
   operation: types.optional(types.enumeration('operation', ['', 'move', 'retire']), ''),
@@ -27,11 +28,42 @@ const WorkflowOutputStore = types.model('WorkflowOutputStore', {
     self.retireTarget = val
   },
 
-  move: flow(function * moveToSubjectSet (subjectIds, subjectSet) {
+  move: flow(function * moveToSubjectSet (subjectIds, subjectSetId) {
     self.operation = 'move'
     self.status = ASYNC_STATES.SENDING
     
-    console.log('+++ MOVE: ', subjectIds, subjectSet)
+    console.log('+++ MOVE: ', subjectIds, subjectSetId)
+    
+    const url = `${apiClient.root}/subject_sets/${subjectSetId}/links/subjects`
+    
+    console.log(apiClient.headers)
+    console.log(url)
+    
+    try {
+      const data = yield superagent
+        .post(url)
+        .withCredentials()
+        .set('Accept', 'application/vnd.api+json; version=1')
+        .set('Authorization', apiClient.headers.Authorization)
+        .set('Content-Type', 'application/json')
+        .send({
+          subjects: subjectIds,
+        })
+        .then(res => {
+          console.log('+++ res ', res)
+          // throw new Error('Workflow Output Store couldn\'t move() data')
+        })
+
+      self.status = ASYNC_STATES.SUCCESS
+      self.statusMessage = undefined
+      
+    } catch (err) {
+      const message = err && err.toString() || undefined
+      self.status = ASYNC_STATES.ERROR
+      self.statusMessage = message
+      console.error('[WorkflowOutputStore] ', err)
+    }
+    
   }),
   
   retire: flow(function * retireInWorkflow (subjectIds, workflowId) {
