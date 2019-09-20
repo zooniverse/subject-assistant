460435b9864e86fc896ff4135fad4d2efdc012df
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-09-20 17:04:50 +0100

    Debugger now pulls projects data fine

--- src/components/Debugger.js
+++ src/components/Debugger.js
@@ -5,9 +5,16 @@ import AppContext from '@store'
 
 function Debugger () {
   const store = useContext(AppContext)
+  const ur = store.userResources
   return (
     <article className="debugger">
       <h2>Debugger</h2>
+      <h3>Owned Projects</h3>
+      {ur.ownedProjects.map((project, index) =>
+        <div key={`user-project-${index}`}>
+          {project.id} / {project.display_name}
+        </div>
+      )}
     </article>
   )
 }
