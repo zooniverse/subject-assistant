479b80e2681773881d6a8015d8ae4c9ec583ae0b
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-12-17 13:43:34 +0800

    Add skeleton for createAndMoveToNewSubjectSet

--- src/store/WorkflowOutputStore.js
+++ src/store/WorkflowOutputStore.js
@@ -5,44 +5,51 @@ import apiClient from 'panoptes-client'
 import superagent from 'superagent'
 
 const WorkflowOutputStore = types.model('WorkflowOutputStore', {
-  operation: types.optional(types.enumeration('operation', ['', 'move', 'retire']), ''),
+  operation: types.optional(types.enumeration('operation', ['', 'move', 'retire', 'create']), ''),
   status: types.optional(types.string, ASYNC_STATES.IDLE),
   statusMessage: types.maybe(types.string),
-  
+
   moveTarget: types.optional(types.string, ''),
   retireTarget: types.optional(types.string, ''),
-  
+  createTarget: types.optional(types.string, ''),
+
 }).actions(self => ({
-    
+
   reset () {
     self.operation = ''
     self.status = ASYNC_STATES.IDLE
     self.statusMessage = undefined
-    
+
     self.moveTarget = ''
     self.retireTarget = ''
+    self.createTarget = ''
   },
-  
+
   resetTargets () {
     self.moveTarget = ''
     self.retireTarget = ''
+    self.createTarget = ''
   },
-  
+
   setMoveTarget (val) {
     self.moveTarget = val
   },
-  
+
   setRetireTarget (val) {
     self.retireTarget = val
+  },
+
+  setCreateTarget (val) {
+    self.createTarget = val
   },
 
   move: flow(function * moveToSubjectSet (subjectIds, subjectSetId) {
     self.operation = 'move'
     self.status = ASYNC_STATES.SENDING
     self.statusMessage = undefined
-    
+
     const url = `${apiClient.root}/subject_sets/${subjectSetId}/links/subjects`
-    
+
     try {
       const data = yield superagent
         .post(url)
@@ -54,7 +61,7 @@ const WorkflowOutputStore = types.model('WorkflowOutputStore', {
           subjects: subjectIds,
         })
         .then(res => {
-          if (res.ok) return res.body          
+          if (res.ok) return res.body
           throw new Error()
         })
         .catch(err => {
@@ -69,23 +76,23 @@ const WorkflowOutputStore = types.model('WorkflowOutputStore', {
 
       self.status = ASYNC_STATES.SUCCESS
       self.statusMessage = undefined
-      
+
     } catch (err) {
       const message = err && err.toString() || undefined
       self.status = ASYNC_STATES.ERROR
       self.statusMessage = message
       console.error('[WorkflowOutputStore] ', err)
     }
-    
+
   }),
-  
+
   retire: flow(function * retireInWorkflow (subjectIds, workflowId) {
     self.operation = 'retire'
     self.status = ASYNC_STATES.SENDING
     self.statusMessage = undefined
-    
+
     const url = `${apiClient.root}/workflows/${workflowId}/retired_subjects`
-    
+
     try {
       yield superagent
         .post(url)
@@ -98,7 +105,7 @@ const WorkflowOutputStore = types.model('WorkflowOutputStore', {
           retirement_reason: 'other',
         })
         .then(res => {
-           if (res.ok) return res.body          
+           if (res.ok) return res.body
            throw new Error()
         })
         .catch(err => {
@@ -108,7 +115,7 @@ const WorkflowOutputStore = types.model('WorkflowOutputStore', {
 
       self.status = ASYNC_STATES.SUCCESS
       self.statusMessage = undefined
-      
+
     } catch (err) {
       const message = err && err.toString() || undefined
       self.status = ASYNC_STATES.ERROR
@@ -116,7 +123,15 @@ const WorkflowOutputStore = types.model('WorkflowOutputStore', {
       console.error('[WorkflowOutputStore] ', err)
     }
   }),
-  
+
+  create: flow(function * createAndMoveToSubjectSet (subjectIds, subjectSetName) {
+    self.operation = 'create'
+    self.status = ASYNC_STATES.SENDING
+    self.statusMessage = undefined
+
+    console.log('+++ ', subjectSetName)
+  }),
+
 }))
 
 export { WorkflowOutputStore }
