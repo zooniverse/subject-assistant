f33e6c6d36acc359d4f1f1e9e7102fcf54094683
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-01-04 23:49:48 +0800

    Remove superagent

--- src/store/WorkflowOutputStore.js
+++ src/store/WorkflowOutputStore.js
@@ -2,7 +2,6 @@ import { flow, types } from 'mobx-state-tree'
 import { ASYNC_STATES } from '@util'
 import config from '@config'
 import apiClient from 'panoptes-client'
-import superagent from 'superagent'
 
 const WorkflowOutputStore = types.model('WorkflowOutputStore', {
   operation: types.optional(types.enumeration('operation', ['', 'move', 'retire', 'create']), ''),
@@ -51,28 +50,29 @@ const WorkflowOutputStore = types.model('WorkflowOutputStore', {
     const url = `${apiClient.root}/subject_sets/${subjectSetId}/links/subjects`
 
     try {
-      const data = yield superagent
-        .post(url)
-        // .withCredentials()  // Do NOT use this after Dec 2019, due to changes with the Panoptes API.
-        .set('Accept', 'application/vnd.api+json; version=1')
-        .set('Authorization', apiClient.headers.Authorization)
-        .set('Content-Type', 'application/json')
-        .send({
+      const data = yield fetch(url, {
+        method: 'POST',
+        // credentials: 'include',  // Do not use, due to CORS issues
+        headers: {
+          'Accept': 'application/vnd.api+json; version=1',
+          'Authorization': apiClient.headers.Authorization,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
           subjects: subjectIds,
-        })
-        .then(res => {
-          if (res.ok) return res.body
-          throw new Error()
-        })
-        .catch(err => {
-          const res = (err && err.response) || {}
-          if (res.status === 404) {
-            throw new Error('[404] Subject Set couldn\'t be found. Either that Subject Set doesn\'t exist or you don\'t have access to it. Please confirm that you are logged in to a Zooniverse account with the necessary privileges.')
-          } else if (res.status === 422) {
-            throw new Error('[422] Cannot process Subjects. Some of the selected Subjects might already be part of the target Subject Set.')
-          }
-          throw new Error('Workflow Output Store couldn\'t move() data')
-        })
+        }),
+      }).then(res => {
+        if (res.ok) return res.json()
+        throw new Error()
+      }).catch(err => {
+        const res = (err && err.response) || {}
+        if (res.status === 404) {
+          throw new Error('[404] Subject Set couldn\'t be found. Either that Subject Set doesn\'t exist or you don\'t have access to it. Please confirm that you are logged in to a Zooniverse account with the necessary privileges.')
+        } else if (res.status === 422) {
+          throw new Error('[422] Cannot process Subjects. Some of the selected Subjects might already be part of the target Subject Set.')
+        }
+        throw new Error('Workflow Output Store couldn\'t move() data')
+      })
 
       self.status = ASYNC_STATES.SUCCESS
       self.statusMessage = undefined
@@ -94,24 +94,22 @@ const WorkflowOutputStore = types.model('WorkflowOutputStore', {
     const url = `${apiClient.root}/workflows/${workflowId}/retired_subjects`
 
     try {
-      yield superagent
-        .post(url)
-        // .withCredentials()  // Do NOT use this after Dec 2019, due to changes with the Panoptes API.
-        .set('Accept', 'application/vnd.api+json; version=1')
-        .set('Authorization', apiClient.headers.Authorization)
-        .set('Content-Type', 'application/json')
-        .send({
+      yield fetch(url, {
+        method: 'POST',
+        // credentials: 'include',  // Do not use, due to CORS issues
+        headers: {
+          'Accept': 'application/vnd.api+json; version=1',
+          'Authorization': apiClient.headers.Authorization,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
           subject_ids: subjectIds,
           retirement_reason: 'other',
-        })
-        .then(res => {
-           if (res.ok) return res.body
-           throw new Error()
-        })
-        .catch(err => {
-          const res = (err && err.response) || {}
-          throw new Error('Workflow Output Store couldn\'t retire() data')
-        })
+        }),
+      }).then(res => {
+        if (res.ok) return res.json()
+        throw new Error('Workflow Output Store couldn\'t retire() data')
+      })
 
       self.status = ASYNC_STATES.SUCCESS
       self.statusMessage = undefined
