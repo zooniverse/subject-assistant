0094adf899e83eb835afe986d793f3e1b34b44db
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-08-05 00:39:28 +0800

    Content update: Task Manager page

--- src/components/MLTaskManager/MLTaskManager.js
+++ src/components/MLTaskManager/MLTaskManager.js
@@ -20,14 +20,42 @@ class MLTaskManager extends React.Component {
       
         <div className="info text">
           <p>
-            At this step, you can retrieve the Subject images <b>(from animal camera traps)</b> you submitted to the external ML Service, <b>along with machine-tagged detection data.</b> Using this new machine-tagged data (which indicates the likelihood that your Subject image is <b>empty of animals or otherwise</b>), you can process the Subject images to an output - e.g. moving non-empty Subjects to a new worfklow for volunteers to examine, retiring empty Subjects, or download the list of images (along with their Zooniverse Subject data and the new ML detection data) as a CSV.
+            The ML model will produce a series of predictions on each image indicating whether or not the computer judged an image as empty with an associated confidence score. That confidence score can be used to parse images and determine the output. You have the option to export results to a .csv file, move certain subjects to a new or existing subject set, or retire them from circulation. 
           </p>
-          <p>Notes:</p>
+          <h3>
+            Querying the returned data
+          </h3>
+          <p>
+            The ML Task Request ID was generated in Step 1 and should be the link from which you were directed to reach this page. If you do not see the ID below, you can return to the project and subject set list in Hamlet to find the link and follow it. Select Fetch to view the results.
+          </p>
+          <p>
+            You can now sort the images based on the ML confidence scores. Use the dropdown menus to select the subset of interest and enter the query and confidence level you are comfortable with. 
+          </p>
+          <p>
+            For instance, if you want to retire images that are almost certainly empty, you could select the query 'where every image is &gt;= 75% likely to be empty'. All images that the computer annotated empty with a confidence of 75-100% will be included in this query. If you want to be more aggressive with retiring potentially empty subjects, you could decrease the confidence level to 50% or even 25%. 
+          </p>
+          <p>
+            Note that the number of images in your subject set and the number within your selection criteria are listed below the query boxes. 
+          </p>
+          <h3>
+            Distribution of selected images
+          </h3>
+          <p>
+            Once you've selected a query, you must select the corresponding project in the dropdown below and then your action for the selection. 
+          </p>
           <ul>
-            <li>The <b>ML Task Request ID</b> is the ID of job that you asked the Machine Learning service to perform on your images. You should have received it when you completed Step 1, usually from a direct URL to this page: e.g. <code>https://subject-assistant.zooniverse.org/#/tasks/1000</code></li>
-            <li>A <b>Subject</b> is a unit of data on the Zooniverse platform. A Subject can have many images, but if you're new to the Zooniverse system, you can assume that one image = one Subject.</li>
-            <li>If you want to <b>retire some Subjects</b>, there needs to be a <b>Workflow</b> to be <i>retired from.</i></li>
-            <li>If you want to <b>move some Subjects</b>, there needs to be a <b>Subject Set</b> to be <i>moved to.</i></li>
+            <li>
+              <b>(A) Export to a .csv file</b> &mdash; download the results of the ML model with the associated labels of empty/not empty. This will allow you to check that the model correctly classified images.
+            </li>
+            <li>
+              <b>(B) Move subjects</b> &mdash; transfer the selection into an existing subject set by selecting it in the dropdown.
+            </li>
+            <li>
+              <b>(C) Retire subjects</b> &mdash; Note that if you want to retire subjects, there must be a workflow to be retired from. Select it in the dropdown to remove these subjects from circulation within that workflow. 
+            </li>
+            <li>
+              <b>(D) Create and move to new subject set</b> &mdash; create a new subject set for your project and populate it with the images in your selection. 
+            </li>
           </ul>
         </div>
         
