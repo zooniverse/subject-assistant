6058afcc620dadde7d095b1e96c398a7282ebbd5
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2023-08-05 01:02:00 +0800

    Content Update - Aug 2023 (#228)
    * Header: properly import Zooniverse Logo
    
    * Webpack: add @assets path
    
    * Content update: intro page
    
    * Content update: Hamlet page
    
    * Content update: Task Manager page
    
    * Build app

--- src/components/HamletPage.js
+++ src/components/HamletPage.js
@@ -8,17 +8,28 @@ export default function HamletPage () {
       <h2>Step 1: Send Photos to ML Service</h2>
       <div className="info text">
         <p>
-          You need to access another Zooniverse app - called <b>Hamlet</b> - to send your photos to the Machine Learning Service.
+          You need to access another Zooniverse app called 'Hamlet' to send your photos to the Machine Learning Service.
         </p>
         <ol>
-          <li>Go to <a href={config.hamletUrl}>Hamlet</a> and make sure you're <b>logged in</b> to the same Zooniverse account as the one you used for this app.</li>
-          <li>You should see a list of your <b>Projects.</b> Choose one, then click on the <b>Subject Assistant</b> link.</li>
-          <li>You should now see a list of <b>Subject Sets</b>. Choose one, then click on the <b>Generate</b> button.</li>
-          <li><i>(Clicking that button submits all the photos in that Subject Set to the Machine Learning Service as a "job", and that job is identified by its "ML Task ID".</i></li>
-          <li>Wait a minute or so, then <b>refresh the page.</b></li>
-          <li>The page should now show that your Subject Set export is "Complete", and you'll see an <b>ML Task ID.</b></li>
-          <li>More importantly, the page will now have a <b>link</b> that says <i>"View the results on the Subject Assistant"</i> - clicking that link will take you directly to <Link to="/tasks">Step 2</Link>, with the ML Task ID pre-filled.</li>
+          <li>
+            Go to Hamlet and log in to the same Zooniverse account as the one you used in the Project Builder to create your project. 
+          </li>
+          <li>
+            You should see a list of your Projects. Choose one, then click on the relevant Subject Assistant link (<b>Camera Traps</b> for wildlife; <b>Zoobot</b> for galaxies; or <b>AutoML</b> if you want to train your own ML algorithm via Google's service).
+          </li>
+          <li>
+            You should now see a list of subject sets within your project. Click the Generate button next to the subject set you'd like to analyze with ML. (Clicking that button submits all the photos in that subject set to the Machine Learning Service as a "job", and that job is identified by its "ML Task ID".)
+          </li>
+          <li>
+            Wait several minutes, then <b>refresh the page.</b>
+          </li>
+          <li>
+            The page should now show that your Subject Set export is "Complete", and you'll see an ML Task ID.
+          </li>
         </ol>
+        <p>
+          Once you have an ML Task ID for your job, you can click on that link to take you directly to Step 2 with the job ID pre-filled. 
+        </p>
       </div>
       <ul className="nav-list">
         <li><a href={config.hamletUrl} target="_blank">Go to Hamlet</a> (do this first)</li>
--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -6,40 +6,50 @@ export default function IntroPage () {
     <article>
       <h2>Introduction</h2>
       <div className="info text">
-        <h3>What?</h3>
         <p>
-          The Zooniverse Machine Learning Subject Assistant is an app that helps <b>ecological researchers</b> on the <a href="https://www.zooniverse.org" target="_blank">Zooniverse</a> platform to send their camera trap images to a Microsoft-powered Machine Learning (ML) service, which is trained to identify whether an image contains any wildlife.
+          The Zooniverse Machine Learning Subject Assistant is an app designed to help researchers integrate machine learning (ML) to speed up the process of reviewing and classifying large datasets. 
+        </p>
+        <p>
+          Combining ML with volunteer effort can increase efficiency and decrease the time to classification for many datasets. Instead of asking volunteers to classify every image, researchers can selectively target the images that need human review. For example, ecology projects can quickly filter out images devoid of animals and ask humans to spend their time extracting data about wildlife from the remaining images.
+        </p>
+        <p>
+          Zooniverse currently offers this service to ecology and conservation projects using camera trap images to monitor wildlife and astrophysics projects using telescope images to classify galaxy morphologies. 
+        </p>
+        <p>
+          <b>Ecologists</b> can run Microsoft's MegaDetector object detection model, which was designed to filter out 'empty' or 'blank' images (those without animals in them) across a wide variety of terrestrial habitats and biomes. 
+        </p>
+        <p>
+          <b>Astrophysicists</b> also have a detection model, <i>though details will be added to this page later.</i>
         </p>
         <p>
           Using this ML-enriched data, researchers can then decide whether to:
         </p>
         <ol>
-          <li>retire Subjects on the Zooniverse (useful for removing "empty of wildlife" images from the pool shown to Zooniverse volunteers)</li>
-          <li>move Subjects to a new Zooniverse Subject Set (useful for placing "hard for to identify" images into a pool earmarked for further human effort), or</li>
-          <li>download the ML-enriched data as a CSV file for further processing/research.</li>
+          <li>
+            Retire subjects from circulation on the Zooniverse (useful for removing "empty of wildlife" images from the pool shown to Zooniverse volunteers)
+          </li>
+          <li>
+            Move subjects to a new Zooniverse Subject Set (useful for placing "hard to identify" images into a pool earmarked for further human effort), or
+          </li>
+          <li>
+            Download the ML-enriched data as a .csv file for further processing/research.
+          </li>
         </ol>
-        
-        <h3>Why?</h3>
         <p>
-          Machine Learning and AI tools can make the make the process of crowd-sourced research much more <i>efficient.</i>
+          Researchers in either domain can also export images to Google's AutoML service to train a new ML algorithm to identify images within their study system. 
+        </p>
+        <h3>
+          Ready to get started?
+        </h3>
+        <p>
+          First, you must create a Zooniverse project and upload your images into a Zooniverse 'Subject Set'. Then you can proceed to:
         </p>
         <p>
-          For example, instead of asking 100 human volunteers to look at 1,000,000 images to identify the animals in the photos, a researcher can use a machine to quickly filter out photos which are <i>almost certainly devoid of any animals,</i> and only ask humans to look at the remaining photos which are <i>"difficult for machines, easy for people"</i> to identify.
+          <Link to="/send">Step 1:</Link> Access the external ML service via the Zooniverse gateway program 'Hamlet' and select the subject set you want to analyze with ML. 
         </p>
-    
-        <h3>Who?</h3>
         <p>
-          This app is designed for <a href="https://www.zooniverse.org" target="_blank">Zooniverse</a> <b>project owners,</b> who have ecological (wildlife) research projects that use <b>camera trap images.</b> For example: <a href="https://www.zooniverse.org/projects/zooniverse/snapshot-serengeti" target="_blank">Snapshot Serengeti.</a>
+          <Link to="/tasks">Step 2:</Link> Retrieve the machine-tagged data and decide which subjects to retire, move, or download. 
         </p>
-    
-        <h3>How?</h3>
-        <p>
-          There are a two major steps, assuming you've already created a Zooniverse <b>Project</b> and uploaded several wildlife camera trap photos as <b>Subjects</b>, organised into <b>Subject Sets.</b>
-        </p>
-        
-        <p><Link to="/send">Step 1:</Link> you choose a <b>Subject Set</b> from your Project to submit to the external ML Service. You'll be doing this via another Zooniverse app called Hamlet, and the process might take a short while.</p>
-        
-        <p><Link to="/tasks">Step 2:</Link> using the machine-tagged data from the ML Service, you choose which Subjects to retire, move, or download. You'll be doing this via this Subject Assistant app.</p>
       </div>
       <ul className="nav-list">
         <li><Link className="go-back" to="/">Back</Link></li>
